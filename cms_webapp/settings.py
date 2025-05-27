473c5a70358db822dd081702556c8d0c3bda462d
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2025-05-27 11:54:45 +1000

    attempts to correct db error

--- cms_webapp/settings.py
+++ cms_webapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,7 +87,7 @@ DATABASES = {
     # }
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "cms_webapp_db",
+        "NAME": os.path.join(BASE_DIR, "cms_webapp_db"),
     }
 }
 
